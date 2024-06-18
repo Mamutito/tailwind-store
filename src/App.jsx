f55c80af6dc0961a2819f106ef471a87830db8a1
Mamutito <aithor123@gmail.com>
2024-06-18 17:53:29 +0100

    feat: card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
+import Card from "./components/Card";
 import Nav from "./components/Nav";
 import ShoeDetail from "./components/ShoeDetail";
+import { SHOE_LIST } from "./constants";
 
 function App() {
   return (
     <main className="animate-fadeIn p-10 xl:px-20">
       <Nav />
       <ShoeDetail />
+      <Card item={SHOE_LIST[0]} />
     </main>
   );
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+const Card = ({ item }) => {
+  return (
+    <article
+      className={`${item.className} max-w-xl transform cursor-pointer transition hover:scale-105`}
+    >
+      <div className="p-8">
+        <h3 className="text-xl font-bold">{item.title}</h3>
+        <p className="mt-10 font-semibold underline underline-offset-4">
+          SHOP NOW +
+        </p>
+      </div>
+      <img className="absolute left-[40%] top-3 w-52" src={item.src} />
+    </article>
+  );
+};
+
+export default Card;
