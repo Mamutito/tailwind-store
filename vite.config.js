be896fd61e757adc6eb8712e15a0479eab410162
Mamutito <aithor123@gmail.com>
2024-06-24 14:45:49 +0100

    chore: base file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/tailwind-store/",
+  base: "./",
   plugins: [react(), svgr()],
 });
